48a36aa271c36b8659d165ef7595bf2936267d43
Daniel Moon <daniel_moon16@yahoo.com>
2024-04-07 18:40:13 -0400

    simplifying build script

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,6 +2,9 @@
 #Exit immediately on error
 set -e
 
+# Set script vars
+DOCKER_TAG="edgar-watcher"
+
 # Build compiled code
 npm install
 rm -rf dist
@@ -9,41 +12,21 @@ npm run build
 rm -rf node_modules
 npm install --only=prod
 
-# Set script vars
-APP_NAME=$(node -p -e "require('./package.json').name")
-npm version patch -f --no-git-tag-version
-APP_VERSION=$(node -p -e "require('./package.json').version")
-DOCKER_TAG="$APP_NAME-$APP_VERSION"
 
-# Build docker image
 echo "Building target for arm64"
 docker buildx build --platform linux/arm64 -t $DOCKER_TAG .
-echo "Build finished"
 
-# Stop old containers
-echo "Stopping all old containers"
-OLD_IMAGES=$(ssh "$PI_USER@$PI_IP" 'docker images --filter "reference=$APP_NAME*" --format "{{.ID}}"')
-for OLD_IMAGE in $OLD_IMAGES; do
-    CONTAINER_IDS=$(ssh "$PI_USER@$PI_IP" "docker ps -q --filter ancestor='$OLD_IMAGE'")
-    for CONTAINER_ID in $CONTAINER_IDS; do
-        ssh "$PI_USER@$PI_IP" "docker stop '$CONTAINER_ID'"
-    done
-done
-echo "Done stopping old containers"
+echo "Stopping old container"
+ssh "$PI_USER@$PI_IP" "docker stop $DOCKER_TAG " || true
 
-# Prune old containers and images
-echo "Pruning all stopped containers and deleting unused images"
-DELETED_ITEMS=$(ssh "$PI_USER@$PI_IP" "docker system prune -a -f")
-echo $DELETED_ITEMS
-echo "Done pruning"
+echo "Removing old container"
+ssh "$PI_USER@$PI_IP" "docker container rm $DOCKER_TAG " || true
 
-# Push image to remote server
-echo "Pushing to remote"
+# echo "Removing old image"
+# ssh "$PI_USER@$PI_IP" "docker image rm $DOCKER_TAG " || true
+
+echo "Pushing new image"
 docker save $DOCKER_TAG | bzip2 | ssh -l $PI_USER $PI_IP docker load
-echo "Done pushing"
 
-
-# Start container with auto restart
 echo "Starting Container"
-STARTED_CONTAINER=$(ssh "$PI_USER@$PI_IP" "docker run -d --restart unless-stopped -p 3000:3000 \"$DOCKER_TAG\"")
-echo "Started container:$STARTED_CONTAINER"
+ssh "$PI_USER@$PI_IP" "docker run -d --network host --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
